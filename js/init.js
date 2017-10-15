4a9c14f8a6fd461c889487aa8122ee2c10fa5f00
Enderson Costa <ecosta@websiteabc.com.br>
2017-10-15 03:32:46 -0200

    Portfólio v0.10
    Primeira etapa do desenvolvimento do portfólio foi feita, apartir de agora basta eu finalizar quando houver tempo.

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.parallax').parallax();
+    $('.carousel').carousel({duration:50});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
